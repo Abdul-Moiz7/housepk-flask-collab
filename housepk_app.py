bafbe5c6f67d6ac085c136fbaa012b1b84bef755
Abdul Moiz7 <moizazaz771@gmail.com>
2025-10-17 17:46:07 +0500

    Add new files for the project

--- housepk_app.py
+++ housepk_app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello from Team HousePK!"
+    return "hello from Team github!"
 
 if __name__ == "__main__":
     app.run(debug=True)
